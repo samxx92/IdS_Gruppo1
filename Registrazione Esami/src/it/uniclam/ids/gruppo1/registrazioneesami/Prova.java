a38cd037bcad6d5fd0f2559144164ad5ce41ffff
samxx92 <samxx92@gmail.com>
2015-11-25 18:30:09 +0100

    Test Push & Pull

--- Registrazione Esami/src/it/uniclam/ids/gruppo1/registrazioneesami/Prova.java
+++ Registrazione Esami/src/it/uniclam/ids/gruppo1/registrazioneesami/Prova.java
@@ -5,6 +5,7 @@ public class Prova {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Ciao Sam");
+		System.out.println("Ciao Cipos");
 	}
 
 }
