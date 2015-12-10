87f61d00d8e4bc4d5e94b1b107bd948e219db8b1
Marcuscip <gianmarcocipolla92@gmail.com>
2015-12-10 14:05:32 +0100

    Confirm Panel

--- Registrazione Esami/src/it/uniclam/ids/gruppo1/registrazioneesami/gui/master/ConfirmPanel.java
+++ Registrazione Esami/src/it/uniclam/ids/gruppo1/registrazioneesami/gui/master/ConfirmPanel.java
@@ -9,7 +9,10 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
 import it.uniclam.ids.gruppo1.registrazioneesami.ClientMainGUI;;
 
@@ -18,8 +21,19 @@ public class ConfirmPanel extends JPanel {
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
 
-	private JTextArea ta = new JTextArea(20, 50);
+	//private JTextArea ta = new JTextArea(20, 50);
+	
+	AbstractTableModel dataModel = new AbstractTableModel() {
+        public int getColumnCount() { return 10; }
+        public int getRowCount() { return 20;}
+        public Object getValueAt(int row, int col) { return new Integer(row*col); }
+    };
+    JTable table = new JTable(dataModel);
+    JScrollPane scrollpane = new JScrollPane(table);
+
 
 	private JButton confirm = new JButton("Conferma");
 
@@ -35,14 +49,14 @@ public class ConfirmPanel extends JPanel {
 		// Campo conferma
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
-		c.gridy = 3;
+		c.gridy = 18;
 		c.gridwidth = 5; // 2 columns wide
 		this.add(confirm, c);
 
 		// Campo esami verbalizzati(label)
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 16;
 		c.gridwidth = 4; // 2 columns wide
 		this.add(new JLabel("Esami Verbalizzati:"), c);
 
@@ -51,9 +65,13 @@ public class ConfirmPanel extends JPanel {
 		c.gridx = 0;
 		c.gridy = 2;
 		c.gridwidth = 8; // 2 columns wide
-		JScrollPane jp = new JScrollPane(ta);
-		this.add(jp, c);
+		c.gridheight = 2;
+		//JScrollPane jp = new JScrollPane(ta);
+		//this.add(jp, c);
+		this.add(table, c);
 
+		
+		
 		confirm.addActionListener(new ActionListener() {
 
 			@Override
