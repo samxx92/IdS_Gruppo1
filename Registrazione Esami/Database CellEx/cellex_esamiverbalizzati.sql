214213c99d0b0e309362f2e7cacf5c9d42da617d
samxx92 <samxx92@gmail.com>
2015-12-17 22:24:35 +0100

    Database CellEx

---
+++ Registrazione Esami/Database CellEx/cellex_esamiverbalizzati.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `cellex` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cellex`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cellex
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `esamiverbalizzati`
+--
+
+DROP TABLE IF EXISTS `esamiverbalizzati`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `esamiverbalizzati` (
+  `id_esame` varchar(45) DEFAULT NULL,
+  `id_docente` varchar(45) DEFAULT NULL,
+  `id_studente` varchar(45) DEFAULT NULL,
+  `voto` int(11) DEFAULT NULL,
+  `data_appello` date DEFAULT NULL,
+  `data_verbalizzazione` date DEFAULT NULL,
+  `id_verbalizzazione` varchar(45) NOT NULL,
+  `scaduto` varchar(45) DEFAULT NULL,
+  `confermato` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_verbalizzazione`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `esamiverbalizzati`
+--
+
+LOCK TABLES `esamiverbalizzati` WRITE;
+/*!40000 ALTER TABLE `esamiverbalizzati` DISABLE KEYS */;
+/*!40000 ALTER TABLE `esamiverbalizzati` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-17 22:23:26
